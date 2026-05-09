534164767461c1f7acf14034d4e415fdee028408
yani1342 <yanissa269@gmail.com>
2026-05-09 15:57:05 +0200

    Update main.cpp

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -14,18 +14,34 @@
 #include "Iter.hpp"
 
 template <typename T>
-void printElem(T &elem)
+void doubleElem(T &elem)
+{
+    elem = elem * 2;
+}
+
+template <typename T>
+void printElem(T const &elem)
 {
     std::cout << elem << std::endl;
 }
 
 int main()
 {
+    // test non-const : on modifie le tableau
     int arr[] = {1, 2, 3, 4, 5};
+    std::cout << "before double:" << std::endl;
+    iter(arr, 5, printElem<int>);
+    iter(arr, 5, doubleElem<int>);
+    std::cout << "after double:" << std::endl;
     iter(arr, 5, printElem<int>);
 
-    std::string strs[] = {"hello", "word"};
-    iter(strs, 2, printElem<std::string>);
+    const int ctab[] = {10, 20, 30};
+    std::cout << "const array:" << std::endl;
+    iter(ctab, 3, printElem<int>);
+
+
+    const std::string strs[] = {"winter", "is", "coming"};
+    iter(strs, 3, printElem<std::string>);
 
     return 0;
 }
